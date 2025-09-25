eb06787b6e93c338b8e80c4a9e5c766b9253f5ee
FranckDD <motadjefranckdonald@gmail.com>
2025-09-25 15:41:44 +0100

    delfileupdate

--- api_backend/app/main.py
+++ api_backend/app/main.py
@@ -33,11 +33,9 @@ app.include_router(users_endpoint.router)
 app.include_router(health_endpoint.router)
 
 if __name__ == "__main__":
-    import uvicorn
-    import os
-
+    import uvicorn, os
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("api_backend.main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
