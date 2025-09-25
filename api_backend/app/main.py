4866ae9c51ce2ba00022ab21d02f4ee8947d55ae
FranckDD <motadjefranckdonald@gmail.com>
2025-09-25 15:56:13 +0100

    persistcorrect1

--- api_backend/app/main.py
+++ api_backend/app/main.py
@@ -35,7 +35,6 @@ app.include_router(health_endpoint.router)
 if __name__ == "__main__":
     import uvicorn, os
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
+    uvicorn.run("api_backend.app.main:app", host="0.0.0.0", port=port)
 
 
