b420493f72f806f5493e8f7a0e9e14b02b1ccb8f
FranckDD <motadjefranckdonald@gmail.com>
2025-09-28 14:42:27 +0100

    apiback11

--- api_backend/backend_app/routes/pharmacy/mapping.py
+++ api_backend/backend_app/routes/pharmacy/mapping.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date
-from backend_app.utils.mapping_general import parse_datetime
+from ...utils.mapping_general import parse_datetime
 
 def normalize_pharmacy_data(raw) -> dict:
     """Normalise les données de pharmacie pour la réponse API"""
